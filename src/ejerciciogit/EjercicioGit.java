f89009054a9c9bd5e189aae098daa190697977e2
ERICK <ERICK@DESKTOP-DL3BQBQ>
2023-02-07 09:55:34 +0100

    Lineas en el metodo main Erick

---
+++ src/ejerciciogit/EjercicioGit.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejerciciogit;
+
+/**
+ *
+ * @author ERICK
+ */
+public class EjercicioGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Erick Nicolas De La Cruz Felipe");
+        System.out.println("Desarro de aplicaciones multiplataforma");
+        System.out.println("“Líneas en el método main”.");
+        
+        
+    }
+    
+}
